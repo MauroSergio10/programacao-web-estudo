a6f2a11309e8b431bcd9b5591dea5bd6526ecb62
MAURO SERGIO CANTUARIA CARMO <mauro.carmo@a.ucb.br>
2024-03-21 10:26:41 -0300

    Atualização da atividade2

--- Atividade 2/verificarNumeroPrimo.js
+++ Atividade 2/verificarNumeroPrimo.js
@@ -1,8 +1,28 @@
-function primoNumber(n){
-    if(n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0){
-        return("Não é primo");
+function verificarNumeroPrimo(n){
+    if(n % 2 == 0){
+        return false;
     }
-    return("É primo");
+
+    if(n == 1){
+        return false
+    }
+
+    for(i = n-1; i > 1; i--){
+        if(n % i == 0){
+            return false
+        }
+    }
+
+    return true;
 }
 
-console.log(primoNumber(148));
+console.log(verificarNumeroPrimo(0)); //false
+console.log(verificarNumeroPrimo(1)) //false
+console.log(verificarNumeroPrimo(2)); //true
+console.log(verificarNumeroPrimo(3)); //true
+console.log(verificarNumeroPrimo(7)); //true
+console.log(verificarNumeroPrimo(83)); //true
+console.log(verificarNumeroPrimo(100)); //false
+console.log(verificarNumeroPrimo(991)); //true
+console.log(verificarNumeroPrimo(104729)); //true
+console.log(verificarNumeroPrimo(14348907)); //false
