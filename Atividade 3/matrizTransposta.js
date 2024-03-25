ae0e3b7ab00ae4728b06b80346cc66d6b67f7f50
Mauro Sérgio <maurosergiocantuaria@gmail.com>
2024-03-25 18:07:05 -0300

    Atividade 3

--- Atividade 3/matrizTransposta.js
+++ Atividade 3/matrizTransposta.js
@@ -1,14 +1,17 @@
-function transporMatriz(matriz){
-    return matriz.map((row, i) => row.map((_, j) => matriz[j][i]));
-}
-
-let matriz = [[1,3,5], 
-              [2,4,6]];
-
-console.log(matriz);
-
-const matrizTransposta = transporMatriz(matriz);
-
-console.log(matrizTransporta);
-
-
+function transporMatriz(matriz) {
+    const matrizTransposta = [];
+  
+    for (let col = 0; col < matriz[0].length; col++) {
+      matrizTransposta.push([]);
+      for (let lin = 0; lin < matriz.length; lin++) {
+        matrizTransposta[col].push(matriz[lin][col]);
+      }
+    }
+  
+    return matrizTransposta;
+  }
+  
+  const matriz = [[1, 3, 5], [2, 4, 6]];
+  console.log(matriz)
+  console.log(transporMatriz(matriz));
+  
